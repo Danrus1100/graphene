1f27c19b4166a0793543df5828f2334337e047d0
trethore <titou.rethore@gmail.com>
2026-07-21 21:01:04 +0200

    refactor(build): improve configuration cache compatibility

--- build-logic/unpack-sources/src/main/kotlin/io/github/trethore/buildlogic/unpacksources/UnpackGitReferencesTask.kt
+++ build-logic/unpack-sources/src/main/kotlin/io/github/trethore/buildlogic/unpacksources/UnpackGitReferencesTask.kt
@@ -2,10 +2,13 @@ package io.github.trethore.buildlogic.unpacksources
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecOperations
+import javax.inject.Inject
 
 abstract class UnpackGitReferencesTask : DefaultTask() {
     @get:Input
@@ -14,11 +17,26 @@ abstract class UnpackGitReferencesTask : DefaultTask() {
     @get:Internal
     abstract val referencesDirectory: DirectoryProperty
 
+    @get:Internal
+    abstract val rootDirectory: DirectoryProperty
+
+    @get:Inject
+    abstract val execOperations: ExecOperations
+
+    @get:Inject
+    abstract val fileSystemOperations: FileSystemOperations
+
     @TaskAction
     fun unpack() {
         val referencesDir = referencesDirectory.get().asFile
+        val rootDir = rootDirectory.get().asFile
         referencesDir.mkdirs()
-        GitReferenceUnpacker(project).unpackAll(
+        GitReferenceUnpacker(
+            logger,
+            rootDir,
+            fileSystemOperations,
+            CommandRunner(execOperations, rootDir),
+        ).unpackAll(
             gitReferences.get().map(GitReference::deserialize),
             referencesDir,
         )
